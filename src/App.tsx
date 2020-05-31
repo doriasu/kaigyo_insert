252b20aadef0d951a2a2d547d881a0b64620fcf9
doriasu <takuandtetsu@live.jp>
2020-05-31 18:45:20 +0900

    メニューの作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
-function App() {
-  return (
-      <div></div>
-  );
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            flexGrow: 1,
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+        },
+        title: {
+            flexGrow: 1,
+        },
+    }),
+);
+
+export default function App() {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        改行コードいれった〜
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </div>
+    );
 }
-
-export default App;
